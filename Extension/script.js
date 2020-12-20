baa5c818fe36bd47ff4187e6617ecf05ab16cc10
crop <bryce@mathx.ca>
2020-12-19 19:34:52 -0800

    Cleanup for Beta 1

--- Extension/script.js
+++ Extension/script.js
@@ -9,7 +9,7 @@ var ticket_flag = false
 var jResultArray = []
 var usernames = []
 
-const htmlEmbed =  '\
+const htmlEmbed = '\
 <div id="myModal" class="modal-crop">\
     <div class="modal-content-crop">\
         <span class="close-crop">&times;</span>\
@@ -19,9 +19,9 @@ const htmlEmbed =  '\
     </div>\
 </div>'
 
-async function getUserData(uname,area_id){
+async function getUserData(uname, area_id) {
     const nickJSON = {
-            "search_nick": uname,
+        "search_nick": uname,
     }
     const battleJSON = {
         "area_id": area_id,
@@ -35,12 +35,12 @@ async function getUserData(uname,area_id){
     }
     apiRequest(query_by_nick, nickJSON).then((data) => {
         console.log(data);
-        if (data.code === 402){
+        if (data.code === 402) {
             console.log("ticket error")
             ticket_flag = true;
             sendMessage("ticket-error")
         }
-        else{
+        else {
             for (var i = 0; i < Object.keys(data.data.player_list).length; i += 1) {
                 if (data.data.player_list[i].area_id === area_id) {
                     var player_data = data.data.player_list[i]
@@ -49,7 +49,7 @@ async function getUserData(uname,area_id){
                     console.log(battleJSON)
                     apiRequest(battle_list, battleJSON).then((battle_data) => {
                         apiRequest(get_battle_topbar_info, battleJSON).then((topbar_data) => {
-                            var jVal = buildJSON(player_data,battle_data,topbar_data)
+                            var jVal = buildJSON(player_data, battle_data, topbar_data)
                             console.log(JSON.stringify(jVal))
                             jResultArray.push(JSON.stringify(jVal))
                         })
@@ -61,9 +61,9 @@ async function getUserData(uname,area_id){
     })
 }
 
-async function profile_request(uname,area_id){
+async function get_profile(uname, area_id) {
     const nickJSON = {
-            "search_nick": uname,
+        "search_nick": uname,
     }
     const battleJSON = {
         "area_id": area_id,
@@ -77,21 +77,21 @@ async function profile_request(uname,area_id){
     }
     apiRequest(query_by_nick, nickJSON).then((data) => {
         console.log(data);
-        if (data.code === 402){
+        if (data.code === 402) {
             console.log("ticket error")
             ticket_flag = true;
             sendMessage("ticket-error")
         }
-        else{
-            for (var i = 0; i < Object.keys(data.data.player_list).length; i += 1) {
+        else {
+            for (let i = 0; i < Object.keys(data.data.player_list).length; i += 1) {
                 if (data.data.player_list[i].area_id === area_id) {
-                    var player_data = data.data.player_list[i]
+                    let player_data = data.data.player_list[i]
                     battleJSON.slol_id = data.data.player_list[i].slol_id
                     console.log("User " + nickJSON.search_nick + " Found on Ionia with slol_id=" + battleJSON.slol_id)
                     apiRequest(battle_list, battleJSON).then((battle_data) => {
                         apiRequest(get_battle_topbar_info, battleJSON).then((topbar_data) => {
                             apiRequest(get_often_used, battleJSON).then((often_used_data) => {
-                                let jRepsonse = profile_basic_builderJSON(battle_data,topbar_data,often_used_data)
+                                let jRepsonse = profile_basic_builderJSON(battle_data, topbar_data, often_used_data)
                                 sendMessage(jRepsonse)
                             })
                         })
@@ -103,8 +103,8 @@ async function profile_request(uname,area_id){
     })
 }
 
-async function get_game_details(slol_id,battle_id,area_id){
-    
+async function get_game_details(slol_id, battle_id, area_id) {
+
     let requestBody = {
         "area_id": parseInt(area_id),
         "battle_id": parseInt(battle_id),
@@ -114,12 +114,12 @@ async function get_game_details(slol_id,battle_id,area_id){
     }
     apiRequest(battle_details, requestBody).then((data) => {
         console.log(data);
-        if (data.code === 402){
+        if (data.code === 402) {
             console.log("ticket error")
             ticket_flag = true;
             sendMessage("ticket-error")
         }
-        else{
+        else {
             let jRepsonse = game_details_builderJSON(data)
             sendMessage(jRepsonse)
         }
@@ -145,51 +145,51 @@ async function apiRequest(myurl, body) {
         });
 }
 
-function buildJSON(user,games,topbar){
+function buildJSON(user, games, topbar) {
     user = Object.assign(games.data, user)
     user = Object.assign(topbar.data, user)
     console.log(user)
     return user;
 }
 
-function profile_basic_builderJSON(battle_data,topbar_data,often_used_data){
-    let player = {"type":"profile-basic-reply"}
+function profile_basic_builderJSON(battle_data, topbar_data, often_used_data) {
+    let player = { "type": "profile-basic-reply" }
     player = Object.assign(topbar_data, player)
     player = Object.assign(often_used_data.data, player)
     player = Object.assign(battle_data.data, player)
     return JSON.stringify(player);
 }
 
-function game_details_builderJSON(jdata){
-    let data = {"type":"profile-detailedmatch-reply"}
+function game_details_builderJSON(jdata) {
+    let data = { "type": "profile-detailedmatch-reply" }
     data = Object.assign(jdata, data)
     return JSON.stringify(data);
 }
 
-function buildMultiMessage(r_data){
-    var m = "";
-    for (i in r_data){
-        if (i === 0){
-            m += i+"="+r_data[i]
+function buildMultiMessage(r_data) {
+    let m = "";
+    for (i in r_data) {
+        if (i === 0) {
+            m += i + "=" + r_data[i]
         }
-        else{
-            m += "&"+i+"="+r_data[i]
+        else {
+            m += "&" + i + "=" + r_data[i]
         }
     }
     return m;
 }
 
 function sendMessage(message) {
-    var receiver = document.getElementById('myiFrame').contentWindow;
+    const receiver = document.getElementById('myiFrame').contentWindow;
     receiver.postMessage(message, 'https://downthecrop.github.io/opgg-clone/');
 }
 
 function main() {
     if (window.location.href.includes("https://www.wegame.com.cn/")) {
         function listCookies() {
-            var theCookies = document.cookie.split(';');
-            var aString = '';
-            for (var i = 1; i <= theCookies.length; i++) {
+            const theCookies = document.cookie.split(';');
+            let aString = '';
+            for (let i = 1; i <= theCookies.length; i++) {
                 aString += i + ' ' + theCookies[i - 1] + "\n";
                 if (theCookies[i - 1].includes("tgp_id")) {
                     loginStatus = true;
@@ -205,13 +205,13 @@ function main() {
         document.body.innerHTML = htmlEmbed + document.body.innerHTML;
 
         //begin GUI injection
-        var topbar = document.getElementsByClassName("widget-header-nav")[0];
+        let topbar = document.getElementsByClassName("widget-header-nav")[0];
         topbar.getElementsByClassName("cur")[0].innerHTML = "<a id='myBtn' href='#'>Cropsearch</a>";
 
         const btn = document.getElementById("myBtn");
         const modal = document.getElementById("myModal");
         const span = document.getElementsByClassName("close-crop")[0];
-        
+
         btn.onclick = function () {
             modal.style.display = "block";
         }
@@ -223,69 +223,68 @@ function main() {
                 modal.style.display = "none";
             }
         }
-        
-        document.getElementById("multi-page").addEventListener('click',function(){
+
+        document.getElementById("multi-page").addEventListener('click', function () {
             document.getElementById("myiFrame").src = "https://downthecrop.github.io/opgg-clone/Multi/";
         })
-        document.getElementById("profile-page").addEventListener('click',function(){
+        document.getElementById("profile-page").addEventListener('click', function () {
             document.getElementById("myiFrame").src = "https://downthecrop.github.io/opgg-clone/Profile/";
         })
     }
 }
 
-(function() {
+(function () {
     var _push = Array.prototype.push;
-    Array.prototype.push = function() {
-      ///this is probably a really bad way to handle async but I don't care for now.
-      _push.apply(this, arguments);
-      if (jResultArray.length != 0 && usernames.length === jResultArray.length){
-        if (ticket_flag === false){
-            message = buildMultiMessage(jResultArray)
-            console.log(message)
-            sendMessage(message)
-            
-            //reset
-            usernames = []
-            jResultArray = []
+    Array.prototype.push = function () {
+        ///this is probably a really bad way to handle async but I don't care for now.
+        _push.apply(this, arguments);
+        if (jResultArray.length != 0 && usernames.length === jResultArray.length) {
+            if (ticket_flag === false) {
+                message = buildMultiMessage(jResultArray)
+                console.log(message)
+                sendMessage(message)
+
+                //reset
+                usernames = []
+                jResultArray = []
+            }
+            else {
+                console.log("Message wasn't send due to ticket error")
+            }
         }
-        else{
-            console.log("Message wasn't send due to ticket error")
-        }
-      }
-      return;
+        return;
     }
 })();
 
-window.addEventListener('message', function(message){
-    if (message.origin === "https://downthecrop.github.io"){
+window.addEventListener('message', function (message) {
+    if (message.origin === "https://downthecrop.github.io") {
         try {
             JSON.parse(message.data)
         } catch (e) {
             console.log("Message isn't json")
             console.log(message.data)
         }
-        //Trycatch passed, just names
-        var jMessage = JSON.parse(message.data)
+        let jMessage = JSON.parse(message.data)
         console.log(jMessage)
-        if (jMessage.type === "name_mutli_search"){
+        if (jMessage.type === "name_mutli_search") {
             sendMessage("loading")
-            var area_id = 31;
-            getUserData(jMessage.user,area_id)
+            let area_id = 31;
+            getUserData(jMessage.user, area_id)
             usernames.push(jMessage.user)
         }
-        if (jMessage.type === "profile-basic"){
+        if (jMessage.type === "profile-basic") {
             //sendMessage("loading")
-            var area_id = 31;
-            profile_request(jMessage.name,area_id)
+            let area_id = 31;
+            get_profile(jMessage.name, area_id)
         }
-        if (jMessage.type === "profile-detailedmatch"){
-            var area_id = 31;
-            get_game_details(jMessage.slol_id,jMessage.battle_id,area_id)
+        if (jMessage.type === "profile-detailedmatch") {
+            let area_id = 31;
+            get_game_details(jMessage.slol_id, jMessage.battle_id, area_id)
         }
     }
 });
 
-var checkExist = setInterval(function () {
+let checkExist = setInterval(function () {
     if ($('.widget-header-nav').length) {
         main()
         clearInterval(checkExist);
