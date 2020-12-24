16eb64562e0b440c1fcbd35b2f9336bbc7266fd0
crop <bryce@mathx.ca>
2020-12-24 01:06:39 -0800

    add res download script

---
+++ res/res-download.py
@@ -0,0 +1,25 @@
+import wget
+import pathlib
+#url = "https://wegame.gtimg.com/g.26-r.c2d3c/helper/lol/assis/images/resources/champions/"
+#url = "https://wegame.gtimg.com/g.26-r.c2d3c/helper/lol/assis/images/resources/items/"
+#url = "https://wegame.gtimg.com/g.26-r.c2d3c/helper/lol/assis/images/resources/summonability/"
+url = "https://wegame.gtimg.com/app/lol/score_souces/division/"
+
+path = str(pathlib.Path(__file__).parent.absolute())+"/"
+
+#i = 515
+i = 0
+#z = 0
+while (i < 100):
+    print(i)
+    try:
+        wget.download(url+str(i)+'.png', path+str(i)+'.png')
+        '''
+        while (z < 4):
+            wget.download(url+str(i)+'_'+str(z)+'.png', 'C:/LOLIMG/'+str(i)+'_'+str(z)+'.png')
+            z += 1
+        '''
+    except:
+        print("error")
+   
+    i += 1
